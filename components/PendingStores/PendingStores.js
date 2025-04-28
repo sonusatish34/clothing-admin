ecf6294e43a34f2bbd35d8ce79f4d27df4fb3f38
sonusatish34 <sonusatish34@gmail.com>
2025-04-28 15:27:22 +0530

    link detail

--- components/PendingStores/PendingStores.js
+++ components/PendingStores/PendingStores.js
@@ -5,19 +5,21 @@ const ComponentName = (props) => {
     const StoreCard = ({ }) => {
         return (
             <div className='pt-6'>
-                <ul className='flex flex-col gap-y-2 border-2 border-[#F5F5F5] rounded-lg'>
-                    <li>
-                        <Image
-                            src="/images/ooo.png"
-                            alt="line"
-                            width={500}
-                            height={300}
-                            className='rounded-t-lg object-cover w-full ' />
-                    </li>
-                    <li className='pl-4'>KLM Shopping Mall, Medipally</li>
-                    <li className='pl-4'>Store Id : 487987747</li>
-                    <li className='pl-4 pb-4'>Submitted At 7 : 32pm, 24may</li>
-                </ul>
+                <Link href={'/pending-stores-detailing'}>
+                    <ul className='flex flex-col gap-y-2 border-2 border-[#F5F5F5] rounded-lg'>
+                        <li>
+                            <Image
+                                src="/images/ooo.png"
+                                alt="line"
+                                width={500}
+                                height={300}
+                                className='rounded-t-lg object-cover w-full ' />
+                        </li>
+                        <li className='pl-4'>KLM Shopping Mall, Medipally</li>
+                        <li className='pl-4'>Store Id : 487987747</li>
+                        <li className='pl-4 pb-4'>Submitted At 7 : 32pm, 24may</li>
+                    </ul>
+                </Link>
             </div>
         );
     }
