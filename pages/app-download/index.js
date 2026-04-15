533f0cc90df1bee60139064fc25031623743d1be
sonusatish34 <sonusatish34@gmail.com>
2026-04-15 14:39:42 +0530

    sw

--- pages/app-download/index.js
+++ pages/app-download/index.js
@@ -1,11 +1,63 @@
 import React from 'react';
-
+import Image from 'next/image';
 const AppDownload = (props) => {
+  // return (
+  //   <div>
+  //     <Image src={'/images/Dozzy123.webp'} width={500} height={500} className=''/>
+  //     <Image src={'/images/app-store.svg'} width={500} height={500} className=''/>
+  //     <Image src={'/images/play-store.png'} width={500} height={500} className=''/>
+  //   </div>
+  // );
+ 
   return (
-    <div>
-      Google store and app store
+  <div className="fixed inset-0 z-[9999] bg-gradient-to-br from-white via-green-50 to-blue-50 flex items-center justify-center px-6">
+    <div className="text-center">
+
+      {/* App Icon */}
+      <div className="mx-auto rounded-3xl flex items-center justify-center mb-5 p-4">
+        <span className="text-white text-3xl font-extrabold tracking-wide">
+           <Image src={'/images/Dozzy123.webp'} width={500} height={500} className=''/>
+        </span>
+      </div>
+
+      {/* Brand Name */}
+      {/* <h1 className="text-3xl font-bold text-gray-900 tracking-wide">
+        Dozzy
+      </h1> */}
+
+      <p className="text-sm text-gray-500 mt-2 mb-6">
+        Please Click Below To Download App
+      </p>
+
+      {/* Download Buttons */}
+      <div className="flex flex-col gap-6 items-center">
+
+        <a
+          href="https://play.google.com/store/apps/details?id=com.dozzy_customer"
+          className=""
+        >
+          <img
+            src="/images/play-store.png"
+            alt="Google Play"
+            className="h-20 w-72 object-contain"
+          />
+        </a>
+
+        <a
+          href="https://apps.apple.com/app/dozzy-farmhouse-rental/id6670319530"
+          className=""
+        >
+          <img
+            src="/images/app-store.svg"
+            alt="App Store"
+            className="h-20 w-72 object-contain"
+          />
+        </a>
+
+      </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default AppDownload;
--- pages/download/index.js
+++ pages/download/index.js
@@ -10,18 +10,12 @@ export default function InstallPage() {
       window.location.href =
         "https://play.google.com/store/apps/details?id=com.dozzy_customer";
     }
-//       https://play.google.com/store/apps/details?id=com.dozzy_customer&pcampaignid=web_share
-
-    // iPhone / iPad
-
 
     else if (/iPad|iPhone|iPod/.test(ua)) {
       window.location.href =
         "https://apps.apple.com/app/dozzy-farmhouse-rental/id6670319530";
-      // https://apps.apple.com/in/app/dozzy-farmhouse-rental/id6670319530
     }
 
-    // Desktop fallback
     else {
       // window.location.href = " http://localhost:3001/app-download";
     }
